cf87862cb176a3e318ef1a523d6b08244b65a46a
liuyafei <543296994@qq.com>
2017-03-02 16:53:44 +0800

    第三次提交

--- app/src/main/java/com/example/liuyafei/myapplication/MainActivity.java
+++ app/src/main/java/com/example/liuyafei/myapplication/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show();
 
         Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show();  Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show();  Toast.makeText(this,"aaaa",Toast.LENGTH_SHORT).show();
     }
 }
